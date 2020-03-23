033775326a0e1149ce3b94d70174d04c71a2cee7
yaofrankie <15900617310@163.com>
2020-03-23 10:55:18 +0800

    Create Spring Boot project.

---
+++ src/main/java/com/frankie/playspringboot/PlaySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.frankie.playspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frankie/playspringboot/PlaySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankie.playspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
